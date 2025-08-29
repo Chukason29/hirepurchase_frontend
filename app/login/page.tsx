eaf0c738794edde88827de4349ec828c51e6fbee
Papi-01 <nnamabrahamchidera@gmail.com>
2025-08-29 06:14:15 +0100

    endpoint

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,8 @@
 "use client";
-
-import logo from "@/public/images/hire-purchase-logo.png";
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 import { FiEye, FiEyeOff } from "react-icons/fi";
-import registerImage from "@/public/images/reg-image.png";
 import { useForm } from "react-hook-form";
 import { loginSchema, type LoginSchema } from "@/utils/Validator";
 import { toast } from "react-toastify";
