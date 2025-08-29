561f50789fd96b2f0efc1916cca7b165006276ec
Papi-01 <nnamabrahamchidera@gmail.com>
2025-08-29 13:45:08 +0100

    clean up

--- app/page.tsx
+++ app/page.tsx
@@ -150,7 +150,11 @@ export default function RegisterPage() {
                   onClick={() => setShowPassword(!showPassword)}
                   className="absolute right-3 top-2.5 text-gray-500 hover:text-gray-700"
                 >
-                  {showPassword ? <FiEyeOff size={20} /> : <FiEye size={20} />}
+                  {showPassword ? (
+                    <FiEyeOff size={20} className="cursor-pointer" />
+                  ) : (
+                    <FiEye size={20} className="cursor-pointer" />
+                  )}
                 </button>
               </div>
 
