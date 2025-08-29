0730959b27901127629feb90b4579801944eddfc
Papi-01 <nnamabrahamchidera@gmail.com>
2025-08-29 13:47:46 +0100

    clean up

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -131,9 +131,9 @@ export default function Login() {
                     className="absolute right-3 top-2.5 text-gray-500 hover:text-gray-700"
                   >
                     {showPassword ? (
-                      <FiEyeOff size={20} />
+                      <FiEyeOff size={20} className="cursor-pointer" />
                     ) : (
-                      <FiEye size={20} />
+                      <FiEye size={20} className="cursor-pointer" />
                     )}
                   </button>
                 </div>
